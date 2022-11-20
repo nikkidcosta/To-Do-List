dfdcd50f9fd383ef440b0e8af203b9ee384580c4
nikkidcosta <90878395+nikkidcosta@users.noreply.github.com>
2022-11-19 20:25:16 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+import Checkbox from '@mui/material/Checkbox';
+import './style.css';
+
+export default function App() {
+  const [tasks, setTasks] = React.useState([
+    createDataForTable(1, 2, 3, 4, 5, 6),
+  ]);
+
+  function createDataForTable(
+    title,
+    description,
+    deadline,
+    priority,
+    isComplete,
+    action
+  ) {
+    return { title, description, deadline, priority, isComplete, action };
+  }
+
+  function addATask(task) {
+    setTasks((a) => a.concat([task]));
+  }
+
+  function deleteATask(title) {
+    const removeItem = tasks.filter((task) => {
+      task.title !== title;
+    });
+    setTasks(removeItem);
+  }
+
+  function updateATask(task) {}
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 52 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              FRAMEWORKS
+            </Typography>
+            <Button
+              color="inherit"
+              onClick={() => addATask(createDataForTable(1, 2, 3, 4, 5, 6))}
+            >
+              Add
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Deadline</TableCell>
+              <TableCell align="right">Priority</TableCell>
+              <TableCell align="right">Is Complete</TableCell>
+              <TableCell align="right">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tasks.map((task) => (
+              <TableRow
+                key={task.title}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {task.title}
+                </TableCell>
+                <TableCell align="right">{task.description}</TableCell>
+                <TableCell align="right">{task.deadline}</TableCell>
+                <TableCell align="right">{task.priority}</TableCell>
+                <TableCell align="right">
+                  <Checkbox
+                    defaultChecked
+                    color="default"
+                    inputProps={{ 'aria-label': 'checkbox with default color' }}
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  <Button
+                    color="inherit"
+                    onClick={() =>
+                      updateATask(createDataForTable(1, 2, 3, 4, 5, 6))
+                    }
+                  >
+                    Update
+                  </Button>
+                  <br></br>
+                  <Button
+                    color="inherit"
+                    onClick={() => deleteATask(task.title)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# To-Do-List
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-wtmefe)
